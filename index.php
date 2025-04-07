9914d4d384293484a3a6aca2f29a8d88a0afec7e
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-07 22:13:04 +0500

    header

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-hello
+<?php $base_url  = "https://stage.reticlewebmarketing.com/shadowandstorm"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shadow and Storm</title>
+    <link rel="icon" href="images/logo1.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- CSS Files
+    ================================================== -->
+    <link href="<?php echo $base_url ?>/css/style.css" rel="stylesheet" type="text/css" >
+
+
+</head>
+<body>
+
+<?php include('nav.php'); ?>
+
+
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,27 @@
+<header>
+  <div class="logo">Shadow & Storm</div>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="the-cycle.php">The Cycle</a></li>
+      <li class="dropdown">
+        <a href="tracks.php" class="dropbtn">Tracks</a>
+        <ul class="dropdown-content">
+          <li><a href="track1.php">01 - Welcome to the Shadow</a></li>
+          <li><a href="track2.php">02 - Rage into Light</a></li>
+          <li><a href="track3.php">03 - The Fire Remains</a></li>
+          <li><a href="track4.php">04 - The Signal Bleeds</a></li>
+          <li><a href="track5.php">05 - Beneath the Static</a></li>
+          <li><a href="track6.php">06 - Shadows Dancing</a></li>
+          <li><a href="track7.php">07 - Annihilation Dream</a></li>
+          <li><a href="track8.php">08 - Lost is the Child</a></li>
+          <li><a href="track9.php">09 - Becoming</a></li>
+          <li><a href="track10.php">10 - Dyssamon’s Fall</a></li>
+        </ul>
+      </li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="signal.php">The Signal</a></li>
+    </ul>
+  </nav>
+</header>
