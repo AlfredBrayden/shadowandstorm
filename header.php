8b71aba23f77a09c3f1acbd322422a6d7a19ff08
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-08 01:22:16 +0500

    format

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php $base_url  = "https://stage.reticlewebmarketing.com/shadowandstorm"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/logo1.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- CSS Files
+    ================================================== -->
+    <link href="<?php echo $base_url ?>/css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
--- index.php
+++ index.php
@@ -1,23 +1,7 @@
-<?php $base_url  = "https://stage.reticlewebmarketing.com/shadowandstorm"; ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shadow and Storm</title>
-    <link rel="icon" href="images/logo1.png" type="image/gif" sizes="16x16">
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<?php include('header.php'); ?>
 
-    <!-- CSS Files
-    ================================================== -->
-    <link href="<?php echo $base_url ?>/css/style.css" rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-
+<title>Shadow and Storm</title>
 
 </head>
 
