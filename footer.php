2c4cab69887cb3b6ad37d24b87feb45e2a74b2dd
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-08 19:23:46 +0500

    footer updated

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+
+<footer>
+    <p>© 2025 Chris Gabriel. All content, including lyrics, text, and design, is protected under U.S. Copyright law. All rights reserved. All lyrics are officially registered with the U.S. Copyright Office. Unauthorized use or reproduction of this material will result in legal action.</p>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
