d80ecf05d2eefcd8b60c2f6e8fbdb14ed8107b4a
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-09 22:13:52 +0500

    updated track

--- nav.php
+++ nav.php
@@ -1,39 +1,44 @@
 <header class="bg-dark border-bottom border-secondary">
-  <nav class="navbar navbar-expand-lg navbar-dark container">
-    <a class="navbar-brand fw-bold text-warning" href="<?php echo $base_url ?>">Shadow & Storm</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="navbar navbar-expand-lg navbar-dark container">
+        <a class="navbar-brand fw-bold text-warning" href="<?php echo $base_url ?>">Shadow & Storm</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="the-cycle.php">The Cycle</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="tracks.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
-            Tracks
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="track1.php">01 - Welcome to the Shadow</a></li>
-            <li><a class="dropdown-item" href="track2.php">02 - Rage into Light</a></li>
-            <li><a class="dropdown-item" href="track3.php">03 - The Fire Remains</a></li>
-            <li><a class="dropdown-item" href="track4.php">04 - The Signal Bleeds</a></li>
-            <li><a class="dropdown-item" href="track5.php">05 - Beneath the Static</a></li>
-            <li><a class="dropdown-item" href="track6.php">06 - Shadows Dancing</a></li>
-            <li><a class="dropdown-item" href="track7.php">07 - Annihilation Dream</a></li>
-            <li><a class="dropdown-item" href="track8.php">08 - Lost is the Child</a></li>
-            <li><a class="dropdown-item" href="track9.php">09 - Becoming</a></li>
-            <li><a class="dropdown-item" href="track10.php">10 - Dyssamon’s Fall</a></li>
-          </ul>
-        </li>
-       <!--  <li class="nav-item"><a class="nav-link" href="about.php">About</a></li> -->
-        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-        <li class="nav-item"><a class="nav-link" href="signal.php">The Signal</a></li>
-      </ul>
-    </div>
-  </nav>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="the-cycle.php">The Cycle</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="tracks.php" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown">
+                        Tracks
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+
+                        <li><a class="dropdown-item" href="01_the_oath.php">01 - The Oath</a></li>
+                        <li><a class="dropdown-item" href="02_welcome_to_the_shadow.php">02 - Welcome to the
+                                Shadow</a></li>
+                        <li><a class="dropdown-item" href="03_rage_into_light.php">03 - Rage Into Light</a></li>
+                        <li><a class="dropdown-item" href="04_call_the_storm.php">04 - Call the Storm</a></li>
+                        <li><a class="dropdown-item" href="05_i_am_eye_to_eye.php">05 - I Am (Eye to Eye)</a></li>
+                        <li><a class="dropdown-item" href="06_the_missing_piece.php">06 - The Missing Piece</a></li>
+                        <li><a class="dropdown-item" href="07_annihilation_dream.php">07 - Annihilation Dream</a>
+                        </li>
+                        <li><a class="dropdown-item" href="08_the_sand.php">08 - The Sand</a></li>
+                        <li><a class="dropdown-item" href="09_the_last_verse.php">09 - The Last Verse</a></li>
+                        <li><a class="dropdown-item" href="10_becoming.php">10 - Becoming</a></li>
+
+                    </ul>
+                </li>
+                <!--  <li class="nav-item"><a class="nav-link" href="about.php">About</a></li> -->
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="signal.php">The Signal</a></li>
+            </ul>
+        </div>
+    </nav>
 </header>
