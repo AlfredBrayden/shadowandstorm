8e4142b59bc8c376183b46928e240194a4803c58
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-10 22:23:31 +0500

    aa

--- thank-you.php
+++ thank-you.php
@@ -1,1 +1,40 @@
-<p>thank you</p>
+<?php include('header.php'); ?>
+
+<title>Thank You </title>
+
+
+</head>
+
+<body>
+
+    <?php include('nav.php'); ?>
+
+    <div class="single-page-bg track-page">
+
+
+        <div class="container">
+
+
+            <section class="signal-page thank-you">
+
+<div class="text-center">
+    <h2>Thanks For Submitting the Form</h2>
+<p>Thank you for submitting the form. We will get back to you soon.</p>
+<a href="/" class="yellow-btn">Back to Home</a>
+</div>
+
+
+
+            </section>
+
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+    <?php include('footer.php'); ?>
