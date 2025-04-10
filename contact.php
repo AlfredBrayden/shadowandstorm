f496038386d3c252cfe197c9c03c5ddbadd75f1a
AlfredBrayden <alfredbrayden11@gmail.com>
2025-04-10 21:45:33 +0500

    contact

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php include('header.php'); ?>
+
+<title>Contact Us</title>
+  <meta name="description" content="Contact Shadow & Storm for inquiries or feedback. We value your thoughts and creativity." />
+
+</head>
+
+<body>
+
+    <?php include('nav.php'); ?>
+
+    <div class="single-page-bg track-page">
+
+
+        <div class="container">
+
+
+            <section class="signal-page">
+
+
+
+
+
+            </section>
+
+
+
+
+
+        </div>
+
+    </div>
+
+
+
+    <?php include('footer.php'); ?>
